75b1aa675046d23a78813c32081d6824950b6bc2
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-22 13:44:25 +0100

    Add files via upload

---
+++ Customer invoice.py
@@ -0,0 +1,28 @@
+def facture(s,n):
+    f=s+(s/100)*15-(s/100)*2
+    print("Total to be paid to the customer",n,"is :",f,"DH")
+
+n1=str(input("Give the first ana last name of customer n1 :"))
+y=int(input("Give the number of items for the customer n1 :"))
+s1=0
+for i in range (1,y+1):
+        print("Give the price of the item n'",i,":")
+        p = float(input())
+        s1=s1+p
+n2=str(input("Give the first ana last name of customer n2 :"))
+y=int(input("Give the number of items for the customer n2 :"))
+s2=0
+for i in range (1,y+1):
+        print("Give the price of the item n'",i,":")
+        p = float(input())
+        s2=s2+p
+n3=str(input("Give the first ana last name of customer n3 :"))
+y=int(input("Give the number of items for the customer n3 :"))
+s3=0
+for i in range (1,y+1):
+        print("Give the price of the item n'",i,":")
+        p = float(input())
+        s3=s3+p
+facture(s1,n1)
+facture(s2,n2)
+facture(s3,n3)
